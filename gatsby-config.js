d9f4b5d311c6beef55462f05c802a2f918cac2de
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2019-07-27 13:15:32 +0200

    Initial work on APIdocs starter.

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  pathPrefix: "/doc",
+  siteMetadata: {
+    title: `APIdocs documentation`,
+    description: `APIdocs is an opinionated Gatsby template for writing technical documentation. Clone, customize and start documenting.`,
+    lang: `en`,
+    indexAlias: `/getting-started/`
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `@carrotsearch/gatsby-plugin-apidocs`,
+      options: {
+        navigation: `${__dirname}/src/navigation.json`,
+        logo:   `${__dirname}/src/logo.html`,
+        footer: `${__dirname}/src/footer.html`,
+        basePath: "content"
+      }
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#ffaa00`,
+        showSpinner: false
+      }
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-dark-mode`,
+    `@carrotsearch/gatsby-transformer-html`,
+    '@carrotsearch/gatsby-plugin-content-search',
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-catch-links`
+  ]
+};
